c3e7eda584e86dceeb9e0966c285626dd9b5c763
RainbowMellow <christina.nygaard@outlook.com>
2020-01-06 12:39:32 +0100

    database

---
+++ src/privatemoviecollection/dal/database/DatabaseConnector.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package privatemoviecollection.dal.database;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Properties;
+
+/**
+ *
+ * @author zilot
+ */
+public class DatabaseConnector
+{
+           private SQLServerDataSource dataSource;
+    
+    public DatabaseConnector() throws IOException
+    {
+        Properties props = new Properties();
+        props.load(new FileReader("DBSettings.db"));
+        dataSource = new SQLServerDataSource();
+        dataSource.setDatabaseName(props.getProperty("database"));
+        dataSource.setUser(props.getProperty("user"));
+        dataSource.setPassword(props.getProperty("password"));        
+        dataSource.setServerName(props.getProperty("server"));
+    }
+    
+    public Connection getConnection() throws SQLServerException
+    {
+        return dataSource.getConnection();
+    }
+    
+    
+}
