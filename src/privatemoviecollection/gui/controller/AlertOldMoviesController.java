802bc9a45e207cf92c4c7ed5da7ebb0f02a08c1d
RainbowMellow <christina.nygaard@outlook.com>
2020-01-10 10:18:47 +0100

    .

--- src/privatemoviecollection/gui/controller/AlertOldMoviesController.java
+++ src/privatemoviecollection/gui/controller/AlertOldMoviesController.java
@@ -76,7 +76,6 @@ public class AlertOldMoviesController implements Initializable
         int index = oldMovieList.getSelectionModel().getSelectedIndex();
         dm.deleteMovie(movieList.get(index));
         movieNames.remove(index);
-        
     }
 
     @FXML
