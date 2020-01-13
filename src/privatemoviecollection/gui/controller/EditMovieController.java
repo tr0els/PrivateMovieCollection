06ac79e0095881bc9b66b6cf80495707ccf28392
RainbowMellow <christina.nygaard@outlook.com>
2020-01-13 20:57:35 +0100

    Fixed weird error
    It told me that the controllers needed to be moved to a different place

--- src/privatemoviecollection/gui/controller/EditMovieController.java
+++ src/privatemoviecollection/gui/controller/EditMovieController.java
@@ -1,8 +1,3 @@
-﻿/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package privatemoviecollection.gui.controller;
 
 import java.io.File;
@@ -128,7 +123,6 @@ public class EditMovieController implements Initializable
      * @param movie
      * @param datamodel
      */
-    public void transfer(Movie movie, DataModel datamodel)
     {
         movie = currentMovie;
         
--- src/privatemoviecollection/gui/controller/MainViewController.java
+++ src/privatemoviecollection/gui/controller/MainViewController.java
@@ -1,9 +1,5 @@
-﻿/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package privatemoviecollection.gui.controller;
+
 
 import java.awt.Desktop;
 import java.io.File;
