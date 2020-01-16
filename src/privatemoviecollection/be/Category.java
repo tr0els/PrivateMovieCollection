771866abbd8f0ed570ebaa3499160857f56f6782
Troels Klein <troels.klein@gmail.com>
2020-01-16 01:10:17 +0100

    Overriding equals method

--- src/privatemoviecollection/be/Category.java
+++ src/privatemoviecollection/be/Category.java
@@ -65,4 +65,23 @@ public class Category
         return getName();
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Category other = (Category) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 }
